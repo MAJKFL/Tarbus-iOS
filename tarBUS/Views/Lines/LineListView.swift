e994da2c586c9b044a40dd4049ee4a809ca96aec
MAJKFL <kubaflor23@gmail.com>
2021-02-19 23:28:45 +0100

    Release version

--- tarBUS/Views/Lines/LineListView.swift
+++ tarBUS/Views/Lines/LineListView.swift
@@ -61,7 +61,6 @@ struct LineListView: View {
                 }
                 .padding()
             }
-            .navigationViewStyle(StackNavigationViewStyle())
             .navigationTitle("Linie")
         }
         .onAppear {
