1748042cec7d002b3d373b87c16c4c7cb85f9ecc
Jakub Florek <63607600+MAJKFL@users.noreply.github.com>
2021-11-12 18:59:12 +0100

    Added URL+Extensions file

--- tarBUS/App/tarBUSApp.swift
+++ tarBUS/App/tarBUSApp.swift
@@ -68,16 +68,3 @@ struct MainView: View {
         return deeplink
     }
 }
-
-extension URL {
-    func valueOf(_ queryParamaterName: String) -> String? {
-        guard let url = URLComponents(string: self.absoluteString) else { return nil }
-        return url.queryItems?.first(where: { $0.name == queryParamaterName })?.value
-    }
-}
-
-extension URL: Identifiable {
-    public var id: String {
-        self.absoluteString
-    }
-}
